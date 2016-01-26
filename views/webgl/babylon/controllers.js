00ef756d09596b2e97c6d83b17cd53058122562e
QylinFly <18612116114@163.com>
2016-01-26 13:25:11 +0800

    everyting is ok

--- views/webgl/babylon/controllers.js
+++ views/webgl/babylon/controllers.js
@@ -2,6 +2,15 @@
 
 angular.module('angularjsBabylonApp')
     .controller('MainCtrl', function ($scope, $http) {
+
+        //$http.get('models/Teapot.json').success(function(data) {
+        //    return sphere;
+        //});
+        //$.post('Gulugulus/setMenu', {
+        //    map: 'qq',
+        //    test: 'test_lwp'
+        //});
+
         $scope.assimpModelUrl = "models/jeep1.ms3d.json";
         $scope.changeModel = function () {
             if ($scope.assimpModelUrl == "models/interior.3ds.json") {
